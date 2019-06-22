ce97bd06ce17a53624f01af2582431989a770388
cgrossi <cgrossi@gmail.com>
2019-06-22 09:46:50 -0600

    Fixed nav resize bug, tweaked animation

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 ## Features To Add
 
 - Scroll to top button
-- Fixed nav
+- Fixed nav while on desktop
+- About me section
+- Footer links
 
 ## Bugs To Fix
 
 - Animation jitter on menu close
-- Event firing to nav on window resize
+- ~~Event firing to nav on window resize~~
+- Sticky menu not closing on link click
--- js/main.js
+++ js/main.js
@@ -1,33 +1,30 @@
 function toggleMenu() {
-    const nav = document.querySelector('.nav');
-    const ham = document.querySelector('.hamburger-menu');
-    const hamBox = document.querySelector('.hamburger-box');
-    
-        ham.addEventListener('click', (event) => {
-            event.stopPropagation();
-            event.target.setAttribute('style', 'opacity: 0.2; z-index: 3')
-            hamBox.setAttribute('style', 'opacity: 0.2; z-index: 3')
-            nav.classList.add('fadeIn')
-            nav.classList.toggle('show');
-            nav.classList.remove('fadeOut')
-            
-    })
+        const nav = document.querySelector('.nav');
+        const ham = document.querySelector('.hamburger-menu');
+        const hamBox = document.querySelector('.hamburger-box');
 
-    document.querySelector('.exit')
-        .addEventListener('click', (event) => {
-            event.stopPropagation();
-            nav.classList.add('fadeOut')
-            nav.classList.remove('fadeIn')
-           setTimeout(() => {
-            nav.classList.toggle('show');
-            ham.setAttribute('style', 'opacity: 1; z-index: 3')
-            hamBox.setAttribute('style', 'opacity: 1; z-index: 3')
-           }, 1000)
-        })
+        ham.addEventListener('click', event => {
+                event.target.setAttribute('style', 'opacity: 0.2; z-index: 3');
+                hamBox.setAttribute('style', 'opacity: 0.2; z-index: 3');
+                nav.classList.add('fadeIn');
+                nav.classList.toggle('show');
+                nav.classList.remove('fadeOut');
+        });
+
+        document.querySelector('.exit').addEventListener('click', () => {
+                nav.classList.add('fadeOut');
+                nav.classList.remove('fadeIn');
+                setTimeout(() => {
+                        nav.classList.toggle('show');
+                        ham.setAttribute('style', 'opacity: 1; z-index: 3');
+                        hamBox.setAttribute('style', 'opacity: 1; z-index: 3');
+                        nav.classList.remove('fadeOut');
+                }, 700);
+        });
 }
 
 function main() {
-    toggleMenu();
+        toggleMenu();
 }
 
 main();
